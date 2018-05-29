8b089f846b94d560d41ddbccc9144853b5a2f7bc
Sushmita Susheelendra <ssusheel@codeaurora.org>
2018-05-29 14:19:47 -0600

    msm: kgsl: Record the cacheability attribute of ion buffers
    Query whether an ion buffer is cached using the dmabuf API
    dma_buf_get_flags. Record this information in the memdesc
    so that any cache operations requested on the ion buffer
    can be honored.
    
    Change-Id: Ie763047260108a8b5b2073b60d7a6e51408ae543
    Signed-off-by: Sushmita Susheelendra <ssusheel@codeaurora.org>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -31,6 +31,7 @@
 #include <linux/compat.h>
 #include <linux/ctype.h>
 #include <linux/mm.h>
+#include <linux/ion.h>
 #include <asm/cacheflush.h>
 #include <uapi/linux/sched/types.h>
 #include <linux/of_fdt.h>
@@ -2407,6 +2408,7 @@ static long _gpuobj_map_dma_buf(struct kgsl_device *device,
 {
 	struct kgsl_gpuobj_import_dma_buf buf;
 	struct dma_buf *dmabuf;
+	unsigned long flags = 0;
 	int ret;
 
 	/*
@@ -2436,6 +2438,16 @@ static long _gpuobj_map_dma_buf(struct kgsl_device *device,
 
 	if (IS_ERR_OR_NULL(dmabuf))
 		return (dmabuf == NULL) ? -EINVAL : PTR_ERR(dmabuf);
+
+	/*
+	 * ION cache ops are routed through kgsl, so record if the dmabuf is
+	 * cached or not in the memdesc. Assume uncached if dma_buf_get_flags
+	 * fails.
+	 */
+	dma_buf_get_flags(dmabuf, &flags);
+	if (flags & ION_FLAG_CACHED)
+		entry->memdesc.flags |=
+			KGSL_CACHEMODE_WRITEBACK << KGSL_CACHEMODE_SHIFT;
 
 	ret = kgsl_setup_dma_buf(device, pagetable, entry, dmabuf);
 	if (ret)
