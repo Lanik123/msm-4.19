0cdc02833ccdff0aa41a472fb0e40982b185443a
Xiaowen Wu <wxiaowen@codeaurora.org>
2019-11-27 15:49:15 -0500

    disp: msm: dp: add tiled dp-mst mode support
    When 8K monitor is connected, dp-mst driver need to be configured
    to tiled mode that two dp-mst stream will work together for super
    frame display. A super encoder and brige are created to drive
    two dp-mst sub-bridges, and dp-mst driver will select the super
    encoder when 8K mode is selected.
    
    Change-Id: I1ba8797675ff82048c854ac3294425dd558f90ec
    Signed-off-by: Xiaowen Wu <wxiaowen@codeaurora.org>
    Signed-off-by: Abhinav Kumar <abhinavk@codeaurora.org>

--- msm/dp/dp_drm.h
+++ msm/dp/dp_drm.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2017-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2017-2020, The Linux Foundation. All rights reserved.
  */
 
 #ifndef _DP_DRM_H_
@@ -158,6 +158,14 @@ int dp_connector_update_pps(struct drm_connector *connector,
  * @encoder: Pointer to encoder for mst bridge mapping
  */
 int dp_mst_drm_bridge_init(void *display,
+	struct drm_encoder *encoder);
+
+/**
+ * dp_mst_drm_super_bridge_init - initialize mst super bridge
+ * @display: Pointer to private display structure
+ * @encoder: Pointer to encoder for mst bridge mapping
+ */
+int dp_mst_drm_super_bridge_init(void *display,
 	struct drm_encoder *encoder);
 
 /**
