2ae92c75321184b3d0e81b835de633e97feb0e08
Amine Najahi <anajahi@codeaurora.org>
2020-03-11 11:32:40 -0400

    disp: msm: sde: add plane layout check and global coordinate support
    Add plane layout bound check and global coordinate support.
    
    Change-Id: I25f36cd0dd66a7c2292a84137f9417856a0803c1
    Signed-off-by: Amine Najahi <anajahi@codeaurora.org>
    Signed-off-by: Abhinav Kumar <abhinavk@codeaurora.org>

--- msm/sde/sde_plane.c
+++ msm/sde/sde_plane.c
@@ -4146,6 +4146,15 @@ sde_plane_duplicate_state(struct drm_plane *plane)
 
 	__drm_atomic_helper_plane_duplicate_state(plane, &pstate->base);
 
+	/* reset layout offset */
+	if (pstate->layout_offset) {
+		if (pstate->layout_offset > 0)
+			pstate->base.crtc_x += pstate->layout_offset;
+		pstate->property_values[PLANE_PROP_LAYOUT].value =
+				SDE_LAYOUT_NONE;
+		pstate->layout_offset = 0;
+	}
+
 	return &pstate->base;
 }
 
