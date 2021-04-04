a2ee0bf83c50cc55fa6a590b3106f70c0c8a5334
Lanik <daniilt971@gmail.com>
2021-04-04 13:12:16 +0300

    Add ramoops from JasperKernel

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -2243,6 +2243,12 @@ void resume_console(void)
 	console_unlock();
 }
 
+void emergency_unlock_console(void)
+{
+	console_suspended = 0;
+}
+EXPORT_SYMBOL(emergency_unlock_console);
+
 #ifdef CONFIG_CONSOLE_FLUSH_ON_HOTPLUG
 
 /**
