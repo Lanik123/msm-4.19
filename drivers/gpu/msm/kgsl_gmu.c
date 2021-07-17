MotorolaMobilityLLC/kernel-msm

msm: kgsl: Don't allocate GMU dump memory if not used
Older targets used this scratch memory for debug purposes.
Newer targets don't use this memory so don't allocate it.

Change-Id: I4bfae0fc10eedb289c6765a15d95045f46ffb96e
Signed-off-by: Carter Cooper <ccooper@codeaurora.org>

drivers/gpu/msm/kgsl_gmu.c
@@ -555,6 +555,8 @@ static void gmu_memory_close(struct gmu_device *gmu)
  */
 static int gmu_memory_probe(struct gmu_device *gmu, struct device_node *node)
 {
+	struct kgsl_device *device = container_of(gmu, struct kgsl_device, gmu);
+	struct adreno_device *adreno_dev = ADRENO_DEVICE(device);
 	int ret;
 
 	ret = gmu_iommu_init(gmu, node);
@@ -582,11 +584,13 @@ static int gmu_memory_probe(struct gmu_device *gmu, struct device_node *node)
 	}
 
 	/* Allocates & maps GMU crash dump memory */
-	gmu->dump_mem = allocate_gmu_kmem(gmu, GMU_NONCACHED_KERNEL,
-			DUMPMEM_SIZE, (IOMMU_READ | IOMMU_WRITE));
-	if (IS_ERR(gmu->dump_mem)) {
-		ret = PTR_ERR(gmu->dump_mem);
-		goto err_ret;
+	if (adreno_is_a630(adreno_dev)) {
+		gmu->dump_mem = allocate_gmu_kmem(gmu, GMU_NONCACHED_KERNEL,
+				DUMPMEM_SIZE, (IOMMU_READ | IOMMU_WRITE));
+		if (IS_ERR(gmu->dump_mem)) {
+			ret = PTR_ERR(gmu->dump_mem);
+			goto err_ret;
+		}
 	}
 
 	/* GMU master log */

Lanik123/msm-4.19

drivers/gpu/msm/kgsl_gmu.c
@@ -411,6 +411,8 @@ static void gmu_memory_close(struct gmu_device *gmu)
  */
 static int gmu_memory_probe(struct gmu_device *gmu, struct device_node *node)
 {
+	struct kgsl_device *device = container_of(gmu, struct kgsl_device, gmu);
+	struct adreno_device *adreno_dev = ADRENO_DEVICE(device);
 	int ret;
 
 	ret = gmu_iommu_init(gmu, node);
@@ -426,11 +428,13 @@ static int gmu_memory_probe(struct gmu_device *gmu, struct device_node *node)
 	}
 
 	/* Allocates & maps GMU crash dump memory */
-	gmu->dump_mem = allocate_gmu_kmem(gmu, DUMPMEM_SIZE,
-				(IOMMU_READ | IOMMU_WRITE));
-	if (IS_ERR(gmu->dump_mem)) {
-		ret = PTR_ERR(gmu->dump_mem);
-		goto err_ret;
+	if (adreno_is_a630(adreno_dev)) {
+		gmu->dump_mem = allocate_gmu_kmem(gmu, DUMPMEM_SIZE,
+					(IOMMU_READ | IOMMU_WRITE));
+		if (IS_ERR(gmu->dump_mem)) {
+			ret = PTR_ERR(gmu->dump_mem);
+			goto err_ret;
+		}
 	}
 
 	return 0;