7628219fcf46a05904db7bb6d84bf22314bcb9ce
Rajesh Bharathwaj <quic_rabharat@quicinc.com>
2022-05-04 18:34:00 -0700

    disp: msm: sde: Update backlight brightness command
    Updating backlight command(0xC2) with single pulse duty cycle
    while updating the brightness of the panel.
    
    Change-Id: I2a50ccdcaf279366ba4d0ba9511a0c0e038b5bf6
    Signed-off-by: Rajesh Bharathwaj <quic_rabharat@quicinc.com>

--- msm/dsi/dsi_panel.c
+++ msm/dsi/dsi_panel.c
@@ -636,10 +636,11 @@ static int dsi_panel_dcs_set_display_brightness_c2(struct mipi_dsi_device *dsi,
 	u16 brightness = (u16)bl_lvl;
 	u8 first_byte = brightness & 0xff;
 	u8 second_byte = brightness >> 8;
-	u8 payload[8] = {second_byte, first_byte,
+	u8 payload[11] = {second_byte, first_byte,
 		second_byte, first_byte,
 		second_byte, first_byte,
-		second_byte, first_byte};
+		second_byte, first_byte,
+		02, 00, 00};
 
 	return mipi_dsi_dcs_write(dsi, 0xC2, payload, sizeof(payload));
 }
